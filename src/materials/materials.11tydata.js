80d93fc3e9cdb6e13e797acec81630eda6dcae5b
Zhuldyz Bakubay <zhuldyz@Zhuldyzs-MacBook-Air.local>
2026-08-15 21:39:14 +0500

    Fix broken layout on contact/about/research pages - use relative asset paths

--- src/materials/materials.11tydata.js
+++ src/materials/materials.11tydata.js
@@ -1,5 +1,6 @@
 module.exports = {
   layout: "material.njk",
+  navSection: "academy",
   eleventyComputed: {
     permalink: (data) => (data.draft ? false : `/academy/${data.page.fileSlug}/index.html`),
   },
